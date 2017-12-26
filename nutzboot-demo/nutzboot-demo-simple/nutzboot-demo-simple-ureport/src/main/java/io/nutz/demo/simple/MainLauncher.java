79825b78fdf1f3e0fe67e1f8b7bb7c095082d17f
Wendal Chen <wendal1985@gmail.com>
2017-12-26 11:00:00 +0800

    change: ureport的demo演示如何获取ExportManager

--- nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-ureport/src/main/java/io/nutz/demo/simple/MainLauncher.java
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-ureport/src/main/java/io/nutz/demo/simple/MainLauncher.java
@@ -1,10 +1,16 @@
 package io.nutz.demo.simple;
 
 import org.nutz.boot.NbApp;
+import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
+
+import com.bstek.ureport.export.ExportManager;
 
 @IocBean
 public class MainLauncher {
+    
+    @Inject
+    protected ExportManager exportManager;
 
 	public static void main(String[] args) throws Exception {
 		new NbApp().setPrintProcDoc(true).run();
