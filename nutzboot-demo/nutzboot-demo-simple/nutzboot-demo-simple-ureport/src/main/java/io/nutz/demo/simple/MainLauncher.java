b4d1268e832cd0c866741b6f5ef46f36146a9afb
Wendal Chen <wendal1985@gmail.com>
2017-12-26 19:49:09 +0800

    fix: ureport需要从nutz ioc读取BuildinDatasource和ImageProvider的对象

--- nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-ureport/src/main/java/io/nutz/demo/simple/MainLauncher.java
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-ureport/src/main/java/io/nutz/demo/simple/MainLauncher.java
@@ -3,17 +3,26 @@ package io.nutz.demo.simple;
 import org.nutz.boot.NbApp;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 
+import com.bstek.ureport.Utils;
 import com.bstek.ureport.export.ExportManager;
 
-@IocBean
+@IocBean(create = "init")
 public class MainLauncher {
-    
+
+    private static final Log log = Logs.get();
+
     @Inject
     protected ExportManager exportManager;
 
-	public static void main(String[] args) throws Exception {
-		new NbApp().setPrintProcDoc(true).run();
-	}
+    public void init() {
+        log.info("BuildinDatasource count=" + Utils.getBuildinDatasources().size());
+    }
+
+    public static void main(String[] args) throws Exception {
+        new NbApp().setPrintProcDoc(true).run();
+    }
 
 }
--- nutzboot-starter-ureport/src/main/java/org/nutz/boot/starter/ureport/UreportSpringEnvStarter.java
+++ nutzboot-starter-ureport/src/main/java/org/nutz/boot/starter/ureport/UreportSpringEnvStarter.java
@@ -1,9 +1,19 @@
 package org.nutz.boot.starter.ureport;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.servlet.ServletContextEvent;
+import javax.sql.DataSource;
 
 import org.nutz.boot.tools.SpringWebContextProxy;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Mirror;
+
+import com.bstek.ureport.Utils;
+import com.bstek.ureport.definition.datasource.BuildinDatasource;
+import com.bstek.ureport.provider.image.ImageProvider;
 
 @IocBean
 public class UreportSpringEnvStarter extends SpringWebContextProxy {
@@ -18,5 +28,36 @@ public class UreportSpringEnvStarter extends SpringWebContextProxy {
         List<String> names = super.getSpringBeanNames();
         names.remove(selfName + ".props");
         return names;
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        super.contextInitialized(sce);
+        // 把BuildinDatasource统统注册一下
+        List<BuildinDatasource> buildinDatasources = appContext.getBeans(BuildinDatasource.class);
+        if (appContext.getIoc().has("dataSource")) {
+            // 添加一个内置的BuildinDatasource
+            DataSource ds = appContext.getIoc().get(DataSource.class);
+            buildinDatasources.add(new BuildinDatasource() {
+                public String name() {
+                    return "nutztboot.buildin";
+                }
+
+                public Connection getConnection() {
+                    try {
+                        return ds.getConnection();
+                    }
+                    catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }
+        buildinDatasources.addAll(Utils.getBuildinDatasources());
+        Mirror.me(Utils.class).setValue(null, "buildinDatasources", buildinDatasources);
+        // 把ImageProvider也注册一下
+        List<ImageProvider> images = appContext.getBeans(ImageProvider.class);
+        images.addAll(Utils.getImageProviders());
+        Mirror.me(Utils.class).setValue(null, "imageProviders", images);
     }
 }
