17723e46b8837d2df9e1c83dbaa6b9c75eab0db6
蛋蛋 <1719411461@qq.com>
2017-12-28 10:47:23 +0800

    update:给ufloStarter,UruleStarter补充ProcDoc

--- nutzboot-starter-uflo/src/main/java/org/nutz/boot/starter/uflo/UfloServletStarter.java
+++ nutzboot-starter-uflo/src/main/java/org/nutz/boot/starter/uflo/UfloServletStarter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.nutz.boot.annotation.PropDoc;
 import org.nutz.boot.starter.WebServletFace;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,7 +18,43 @@ import com.bstek.uflo.console.UfloServlet;
 @SuppressWarnings("serial")
 @IocBean
 public class UfloServletStarter extends UfloServlet implements WebServletFace {
-    
+
+    @PropDoc(value = "义UFLO中所有表数据主键中的缓冲区大小", defaultValue = "10")
+    public static final String UFLO_IDBLOCKSIZE = "uflo.idBlockSize";
+
+    @PropDoc(value = "是否禁用当前应用中任务提醒功能", defaultValue = "false???")
+    public static final String UFLO_DISABLESCHEDULER = "uflo.disableScheduler";
+
+    @PropDoc(value = "任务提醒Job池的大小", defaultValue = "10")
+    public static final String UFLO_JOBTHREADCOUNT = "uflo.jobThreadCount";
+
+    @PropDoc(value = "是否以Daemon模式开启线程池来运行job", defaultValue = "true")
+    public static final String UFLO_MAKESCHEDULERTHREADDAEMON = "uflo.makeSchedulerThreadDaemon";
+
+    @PropDoc(value = "是否运行在debug模式下", defaultValue = "true")
+    public static final String UFLO_DEBUG = "uflo.debug";
+
+    @PropDoc(value = "用于在任务过期计算时设置一天的时长", defaultValue = "8")
+    public static final String UFLO_BUSINESSDAYHOURS = "uflo.businessDayHours";
+
+    @PropDoc(value = "设置即将过期的任务的阀值", defaultValue = "1440")
+    public static final String UFLO_MINUTESBEFOREDUEDATETOREMIND = "uflo.minutesBeforeDueDateToRemind";
+
+    @PropDoc(value = "设置即将过期的任务的阀值", defaultValue = "false")
+    public static final String UFLO_DISABLEDDEPTASSIGNEEPROVIDER	 = "uflo.disabledDeptAssigneeProvider";
+
+    @PropDoc(value = "是否禁用UFLO当中提供的默认的用于提供给流程模版设计器使用的基于用户的任务处理人分配方式。", defaultValue = "false")
+    public static final String UFLO_DISABLEDUSERASSIGNEEPROVIDER		 = "uflo.disabledUserAssigneeProvider";
+
+    @PropDoc(value = "是否禁用UFLO内部提供的默认的用于显示流程进度图任务节点消息提示功能。", defaultValue = "false")
+    public static final String UFLO_DISABLEDEFAULTTASKDIAGRAMINFOPROVIDER		 = "uflo.disableDefaultTaskDiagramInfoProvider";
+
+    @PropDoc(value = "是否禁用默认流程设计器中的基于文件夹的流程模版存储器", defaultValue = "true")
+    public static final String UFLO_DISABLEDEFAULTFILEPROCESSPROVIDER	= "uflo.disableDefaultFileProcessProvider";
+
+    @PropDoc(value = "基于文件夹的流程模版存储器采用的文件夹", defaultValue = "/WEB-INF/processfiles")
+    public static final String UFLO_DEFAULTFILESTOREDIR	= "uflo.defaultFileStoreDir";
+
     public static final String URL="/uflo";
 
     public String getName() {
--- nutzboot-starter-urule/src/main/java/org/nutz/boot/starter/urule/UruleServletStarter.java
+++ nutzboot-starter-urule/src/main/java/org/nutz/boot/starter/urule/UruleServletStarter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.nutz.boot.annotation.PropDoc;
 import org.nutz.boot.starter.WebServletFace;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,7 +18,26 @@ import com.bstek.urule.console.servlet.URuleServlet;
 @SuppressWarnings("serial")
 @IocBean
 public class UruleServletStarter extends URuleServlet implements WebServletFace {
-    
+
+    @PropDoc(value = "配置当前资源库存放目录", defaultValue = "")
+    public static final String URULE_REPOSITORY_DIR = "urule.repository.dir";
+
+    @PropDoc(value = "在外部指定一个将资源库存储到数据库的配置文件", defaultValue = "")
+    public static final String URULE_REPOSITORY_XML = "urule.repository.xml";
+
+    @PropDoc(value = "客户端上配置的服务器地址", defaultValue = "")
+    public static final String URULE_RESPORITYSERVERURL = "urule.resporityServerUrl";
+
+    @PropDoc(value = "用来指定客户端多久到服务端检查当前知识包有没有更新", defaultValue = "0")
+    public static final String URULE_KNOWLEDGEUPDATECYCLE = "urule.knowledgeUpdateCycle";
+
+    @PropDoc(value = "一个URL，用于替换URule Console主界面第一次看到的工作区内容", defaultValue = "0")
+    public static final String URULE_WELCOMEPAGE = "urule.welcomePage";
+
+    @PropDoc(value = "一个字符串，用来替代URule控制台页面的title", defaultValue = "URule Console")
+    public static final String URULE_CONSOLE_TITLE = "urule.console.title";
+
+
     public String getName() {
         return "urule";
     }
@@ -46,4 +66,5 @@ public class UruleServletStarter extends URuleServlet implements WebServletFace 
             sc.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, pre);
         }
     }
+
 }
