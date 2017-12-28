c0eb73ec63371184da61e2c406598c37ffc84e54
Wendal Chen <wendal1985@gmail.com>
2017-12-28 11:23:36 +0800

    fix: swagger可能乱码,undertow无法与swagger一起工作,druid stat view没打印用户名
    过程是这样的:
    改simple-dao, 加上swagger注解和链接,发现没提示druid用户名
    改simple-sharding-jdbc, 加上swagger注解和链接,发现NutFilterStarter有点挫,改造一下
    改simple-undertow, 加上swagger注解和链接, 发现404和乱码

--- nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-undertow/src/main/java/io/nutz/demo/simple/MainLauncher.java
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-undertow/src/main/java/io/nutz/demo/simple/MainLauncher.java
@@ -5,9 +5,14 @@ import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Ok;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("demo")
 @IocBean
 public class MainLauncher {
 
+    @ApiOperation(value = "获取当前毫秒数", notes="获取当前毫秒数", httpMethod="GET", response=Long.class)
 	@Ok("raw")
 	@At("/time/now")
 	public long now() {
