b91a03056a358327cee5a3f9cd8ebf60b37407c6
Wendal Chen <wendal1985@gmail.com>
2017-12-29 12:04:22 +0800

    update: 更新demo-maker

--- nutzboot-demo/nutzboot-demo-maker/src/main/resources/static/thymeleaf/src/main/java/_package/MainLauncher.java
+++ nutzboot-demo/nutzboot-demo-maker/src/main/resources/static/thymeleaf/src/main/java/_package/MainLauncher.java
@@ -1,28 +1,9 @@
 package [[${params.packageName}]];
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 import org.nutz.boot.NbApp;
 import org.nutz.ioc.impl.PropertiesProxy;
-import org.nutz.ioc.loader.annotation.Inject;
-import org.nutz.ioc.loader.annotation.IocBean;
-import org.nutz.lang.Files;
-import org.nutz.lang.Streams;
-import org.nutz.lang.random.R;
-import org.nutz.lang.util.Disks;
-import org.nutz.lang.util.FileVisitor;
-import org.nutz.lang.util.NutMap;
-import org.nutz.mvc.Mvcs;
-import org.nutz.mvc.adaptor.JsonAdaptor;
-import org.nutz.mvc.annotation.AdaptBy;
-import org.nutz.mvc.annotation.At;
-import org.nutz.mvc.annotation.Ok;
-import org.nutz.mvc.annotation.Param;
+import org.nutz.ioc.loader.annotation.*;
+import org.nutz.mvc.annotation.*;
 
 @IocBean(create="init", depose="depose")
 public class MainLauncher {
@@ -33,12 +14,14 @@ public class MainLauncher {
     @At("/")
     @Ok("->:/index.html")
     public void index() {}
-	
-	public void init() {}
-	public void depose() {}
+    
+    public void init() {
+        // NB自身初始化完成后会调用这个方法
+    }
+    public void depose() {}
 
     public static void main(String[] args) throws Exception {
-        new NbApp(MainLauncher.class).run();
+        new NbApp().run();
     }
 
 }
---
+++ nutzboot-demo/nutzboot-demo-maker/src/main/resources/static/thymeleaf/src/main/java/_package/module/TimeModule.java
@@ -0,0 +1,28 @@
+package [[${params.packageName}]].module;
+
+
+import org.nutz.ioc.impl.PropertiesProxy;
+import org.nutz.ioc.loader.annotation.*;
+import org.nutz.mvc.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("time")
+@At("/time")
+@IocBean(create="init", depose="depose")
+public class TimeModule {
+    
+    @Inject
+    protected PropertiesProxy conf;
+    
+    @ApiOperation(value = "获取当前毫秒数", notes = "服务器端的时间", httpMethod="GET", response=Long.class)
+    @At
+    @Ok("raw")
+    public long now() {
+        return System.currentTimeMillis();
+    }
+    
+    public void init() {}
+    public void depose() {}
+
+}
