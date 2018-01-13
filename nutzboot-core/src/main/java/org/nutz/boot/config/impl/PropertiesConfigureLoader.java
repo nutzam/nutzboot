c1dac72b29b935b70b0a8bce6dd8bd26f43df711
wentao <wentao@mgtv.com>
2018-01-13 22:11:46 +0800

    1 新增支持读取jar包外部application.properties为绝对路径 2 新增支持application.properties配置configDir，可自动加载该目录下所有配置文件

--- nutzboot-core/src/main/java/org/nutz/boot/config/impl/PropertiesConfigureLoader.java
+++ nutzboot-core/src/main/java/org/nutz/boot/config/impl/PropertiesConfigureLoader.java
@@ -3,6 +3,7 @@ package org.nutz.boot.config.impl;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.nutz.ioc.impl.PropertiesProxy;
 import org.nutz.lang.Streams;
@@ -16,6 +17,42 @@ import org.nutz.lang.Streams;
  */
 public class PropertiesConfigureLoader extends AbstractConfigureLoader {
 
+    // 获取应用程序绝对路径
+    private static String getBasePath() {
+        String basePath = PropertiesConfigureLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        int lastIndex = basePath.lastIndexOf(File.separator);
+        basePath = basePath.substring(0, lastIndex);
+        try {
+            basePath = java.net.URLDecoder.decode(basePath, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return basePath;
+    }
+
+    // 根据目录和文件名拼接绝对路径
+    private static String getPath(String... name) {
+        String path = getBasePath();
+        for(int i=0; i< name.length; i++) {
+            path = path + File.separator + name[i];
+        }
+        return path;
+    }
+
+    // 获取指定目录下所有指定扩展名的文件列表
+    private static ArrayList<String> getFiles(String path, String extName) {
+        ArrayList<String> fileNames = new ArrayList<>();
+        File file = new File(path);
+        if(file.isDirectory()) {
+            for(File f : file.listFiles()) {
+                if(f.getName().endsWith(extName)) {
+                    fileNames.add(f.getName());
+                }
+            }
+        }
+        return fileNames;
+    }
+
     public void init() throws Exception {
     	// 首先, 确定一些从什么路径加载配置文件,默认值application.properties
         String path = envHolder.get("nutz.boot.configure.properties_path", "application.properties");
@@ -28,8 +65,9 @@ public class PropertiesConfigureLoader extends AbstractConfigureLoader {
             }
         }
         // 如果当前文件夹存在application.properties,读取之
+        String applicationPath = getPath("application.properties");
         try {
-            File tmp = new File(path);
+            File tmp = new File(applicationPath);
             if (tmp.exists() && tmp.canRead()) {
                 try (FileInputStream ins = new FileInputStream(tmp)) {
                     conf.load(Streams.utf8r(ins), false);
@@ -37,6 +75,23 @@ public class PropertiesConfigureLoader extends AbstractConfigureLoader {
             }
         }
         catch (Throwable e) {
+        }
+        // 如果conf内含有configDir配置，则读取configDir下的所有配置文件
+        // 配置示例： configDir=config, 那么读取的就是jar包当前目录下config子目录下的所有properties文件
+        if(conf.has("configDir")) {
+            String configDir = getPath(conf.get("configDir"));
+            for(String fileName: getFiles(configDir, "properties")) {
+                try {
+                    File tmp = new File(getPath(fileName));
+                    if (tmp.exists() && tmp.canRead()) {
+                        try (FileInputStream ins = new FileInputStream(tmp)) {
+                            conf.load(Streams.utf8r(ins), false);
+                        }
+                    }
+                }
+                catch (Throwable e) {
+                }
+            }
         }
         // 也许命令行里面指定了profile,需要提前load进来
         PropertiesProxy tmp = new PropertiesProxy();
