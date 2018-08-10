1684041132860550155bbc57eaeca3e629c2734a
黄川 <306955302@qq.com>
2018-08-10 11:56:08 +0800

    添加一个nutzbootstart

---
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-email/src/main/java/io/nutz/demo/simple/MainLauncher.java
@@ -0,0 +1,35 @@
+package io.nutz.demo.simple;
+
+import org.apache.commons.mail.ImageHtmlEmail;
+import org.nutz.boot.NbApp;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+
+@IocBean(create = "init")
+public class MainLauncher {
+
+    private static final Log log = Logs.get();
+
+    @Inject
+    ImageHtmlEmail email;
+
+    public static void main(String[] args) throws Exception {
+        new NbApp().setPrintProcDoc(true).run();
+    }
+
+    public void init() {
+        try {
+            //注意垃圾收件箱
+            email.setSubject("NutzBootEmainStarter");
+            email.addTo("306955302@qq.com");
+            email.setHtmlMsg("此邮件是Nutz Boot Email Starter发送给您的测试邮件！");
+            log.debug("开始邮件发送");
+            email.send();
+            log.debug("邮件发送完毕");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
