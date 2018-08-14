7d9d7eedccb122d701e764a19522c616e507b274
黄川 <306955302@qq.com>
2018-08-14 15:43:42 +0800

    add:添加一个可以防止表单重复提交的解决方案名为nutzboot-starter-prevent-duplicate-submit

---
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-prevent-duplicate-submit/src/main/resources/chain/nutzfw-mvc-chain.js
@@ -0,0 +1,15 @@
+var chain = {
+    "default": {
+        "ps": [
+            "org.nutz.mvc.impl.processor.UpdateRequestAttributesProcessor",
+            "org.nutz.mvc.impl.processor.EncodingProcessor",
+            "org.nutz.mvc.impl.processor.ModuleProcessor",
+            "org.nutz.mvc.impl.processor.ActionFiltersProcessor",
+            "org.nutz.mvc.impl.processor.AdaptorProcessor",
+            "org.nutz.boot.starter.nutz.mvc.PreventDuplicateSubmitProcessor",
+            "org.nutz.mvc.impl.processor.MethodInvokeProcessor",
+            "org.nutz.mvc.impl.processor.ViewProcessor"
+        ],
+        "error" : 'io.nutz.demo.simple.FailProcessor'
+    }
+};
---
+++ nutzboot-starter/nutzboot-starter-prevent-duplicate-submit/src/main/java/org/nutz/boot/starter/prevent/duplicate/submit/annotation/Type.java
@@ -0,0 +1,10 @@
+package org.nutz.boot.starter.prevent.duplicate.submit.annotation;
+
+/**
+ * @author 黄川 306955302@qq.com
+ * @date: 2018/8/14
+ * 描述此类：
+ */
+public enum Type {
+    CREATE, REMOVE
+}
