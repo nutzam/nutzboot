21bc9d20b8d1b6da74cc27be3bb654a418a85a2d
Howe Chiang <howechiang@gmail.com>
2018-09-20 12:31:45 +0800

    add: 加个lettuce stater测试下

---
+++ nutzboot-starter/nutzboot-starter-redis-lettuce/src/main/java/org/nutz/boot/starter/redis.lettuce/LettuceStarter.java
@@ -0,0 +1,33 @@
+package org.nutz.boot.starter.redis.lettuce;
+
+import org.nutz.boot.annotation.PropDoc;
+import org.nutz.boot.ioc.IocLoaderProvider;
+import org.nutz.integration.jedis.JedisIocLoader;
+import org.nutz.ioc.IocLoader;
+
+public class JedisStarter implements IocLoaderProvider {
+
+    protected static String PRE = "redis.";
+    @PropDoc(value="redis服务器链接URL", defaultValue="redis://127.0.0.1:6379/0")
+    public static final String PROP_URL = "lettuce.url";
+/*    @PropDoc(value="redis服务器ip或域名", defaultValue="127.0.0.1")
+    public static final String PROP_HOST = PRE + "host";
+    @PropDoc(value="redis服务器端口", defaultValue="6379")
+    public static final String PROP_PORT = PRE + "port";
+    @PropDoc(value="redis读写超时", defaultValue="2000")
+    public static final String PROP_TIMEOUT = PRE + "timeout";
+    @PropDoc(value="redis密码")
+    public static final String PROP_PASSWORD = PRE + "password";
+    @PropDoc(value="redis数据库序号", defaultValue="0")
+    public static final String PROP_DATABASE = PRE + "database";
+    @PropDoc(value="redis集群节点列表")
+    public static final String PROP_NODES = PRE + "nodes";
+    @PropDoc(value="redis写超时", defaultValue="0")
+    public static final String PROP_SO_TIMEOUT = PRE + "soTimeout";
+    @PropDoc(value="redis集群最大重定向次数", defaultValue="10")
+    public static final String PROP_MAX_REDIR = PRE + "max_redir";*/
+    
+    public IocLoader getIocLoader() {
+    	return new JedisIocLoader();
+    }
+}
