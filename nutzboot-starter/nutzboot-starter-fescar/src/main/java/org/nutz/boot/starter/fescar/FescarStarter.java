d68aeb5cf6c16e5b6a092d9ace104dea861bff6b
Wizzercn <wizzer@qq.com>
2019-03-28 22:25:04 +0800

    update: starter-fescar applicationId默认取dubbo.application.name值,取不到再取nutz.application.name

--- nutzboot-starter/nutzboot-starter-fescar/src/main/java/org/nutz/boot/starter/fescar/FescarStarter.java
+++ nutzboot-starter/nutzboot-starter-fescar/src/main/java/org/nutz/boot/starter/fescar/FescarStarter.java
@@ -1,17 +1,18 @@
 package org.nutz.boot.starter.fescar;
 
+import com.alibaba.fescar.common.util.StringUtils;
+import com.alibaba.fescar.rm.RMClient;
+import com.alibaba.fescar.tm.TMClient;
 import org.nutz.boot.AppContext;
 import org.nutz.boot.annotation.PropDoc;
 import org.nutz.boot.starter.ServerFace;
 import org.nutz.ioc.impl.PropertiesProxy;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Lang;
+import org.nutz.lang.Strings;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
-
-import com.alibaba.fescar.common.util.StringUtils;
-import com.alibaba.fescar.rm.RMClient;
-import com.alibaba.fescar.tm.TMClient;
 
 @IocBean
 public class FescarStarter implements ServerFace {
@@ -53,7 +54,10 @@ public class FescarStarter implements ServerFace {
             log.info("Global transaction is disabled.");
             return;
         }
-        applicationId = conf.check(PROP_APPID);
+        applicationId = conf.get(PROP_APPID, conf.get("dubbo.application.name", conf.get("nutz.application.name", "")));
+        if (Strings.isBlank(applicationId)) {
+            throw Lang.makeThrow("Ioc.$conf expect property '%s'", PROP_APPID);
+        }
         txServiceGroup = conf.check(PROP_TXGROUP);
         log.infof("fescar applicationId=%s txServiceGroup=%s", applicationId, txServiceGroup);
 
