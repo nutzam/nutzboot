63ac1ff3ed596680494cc4d5bb3ae250e51bd1e3
Wendal Chen <wendal1985@gmail.com>
2019-04-17 19:38:16 +0800

    add: 重新实现starter-thymeleaf,并添加thymeleaf+shiro的demo
    refer: https://nutz.cn/yvr/t/7gareesackiftr1bgo2tadmobq

--- nutzboot-starter/nutzboot-starter-thymeleaf/src/main/java/org/nutz/boot/starter/thymeleaf/ThymeleafView.java
+++ nutzboot-starter/nutzboot-starter-thymeleaf/src/main/java/org/nutz/boot/starter/thymeleaf/ThymeleafView.java
@@ -1,50 +1,32 @@
 package org.nutz.boot.starter.thymeleaf;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.nutz.lang.util.Context;
-import org.nutz.lang.util.NutMap;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.mvc.Mvcs;
 import org.nutz.mvc.view.AbstractPathView;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-import org.thymeleaf.dialect.IDialect;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 public class ThymeleafView extends AbstractPathView {
 
     private static final Log log = Logs.get();
 
-    private TemplateEngine templateEngine = new TemplateEngine();
+    protected TemplateEngine templateEngine;
 
-    private String contentType;
-
-    private String encoding;
-
-    public ThymeleafView(ClassLoader classLoader, NutMap prop, String path) {
+    protected String contentType;
+    protected String encoding;
+    
+    public ThymeleafView(TemplateEngine templateEngine, String path, String contentType, String encoding) {
         super(path);
-        templateEngine.setTemplateResolver(initializeTemplateResolver(classLoader, prop));
-
-        encoding = prop.getString("encoding", "UTF-8");
-        contentType = prop.getString("contentType", "text/html") + "; charset=" + encoding;
-
-        prop.getList("dialects", String.class).forEach(dialect -> {
-            try {
-                Object obj = Class.forName(dialect).getDeclaredConstructor().newInstance();
-                if (obj instanceof IDialect) {
-                    log.debugf("在 thymeleaf 视图中加载 %s 类。", dialect);
-                    templateEngine.setDialect((IDialect) obj);
-                }
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
-                log.debugf("未在 thymeleaf 视图中加载 %s 类，因为其并不是 org.thymeleaf.dialect.IDialect 合法的实现类。", dialect);
-            }
-        });
+        this.templateEngine = templateEngine;
+        this.contentType = contentType;
+        this.encoding = encoding;
     }
 
     @Override
@@ -64,18 +46,5 @@ public class ThymeleafView extends AbstractPathView {
             log.error("模板引擎错误", e);
             throw e;
         }
-    }
-
-    private ITemplateResolver initializeTemplateResolver(ClassLoader classLoader, NutMap prop) {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver(classLoader);
-
-        templateResolver.setTemplateMode(prop.getString("mode", "HTML"));
-        templateResolver.setPrefix(prop.getString("prefix", "template/"));
-        templateResolver.setSuffix(prop.getString("suffix", ".html"));
-        templateResolver.setCharacterEncoding(prop.getString("encoding", "UTF-8"));
-        templateResolver.setCacheable(prop.getBoolean("cache", true));
-        templateResolver.setCacheTTLMs(prop.getLong("cacheTTLMs", 3600000L));
-
-        return templateResolver;
     }
 }
