42d6df69e71675cca932ef51dde80db65f65e923
Wendal Chen <wendal1985@gmail.com>
2019-05-23 21:52:00 +0800

    add: 添加两个帮助类WebFilterReg和WebServletReg

--- nutzboot-core/src/main/java/org/nutz/boot/starter/WebServletFace.java
+++ nutzboot-core/src/main/java/org/nutz/boot/starter/WebServletFace.java
@@ -3,6 +3,7 @@ package org.nutz.boot.starter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 
@@ -11,9 +12,9 @@ public interface WebServletFace {
     String getName();
 
     String getPathSpec();
-    
+
     default String[] getPathSpecs() {
-        return new String[] {getPathSpec()};
+        return new String[]{getPathSpec()};
     }
 
     Servlet getServlet();
@@ -24,5 +25,17 @@ public interface WebServletFace {
 
     default void setServletContext(ServletContext sc) {
 
+    }
+
+    default boolean isAsyncSupported() {
+        return false;
+    }
+
+    default MultipartConfigElement getMultipartConfig() {
+        return null;
+    }
+
+    default int getLoadOnStartup() {
+        return 1;
     }
 }
