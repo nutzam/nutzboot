6a85a249f1364fa6a77f5a31566bb104e5821fe7
Wendal Chen <wendal1985@gmail.com>
2019-06-17 21:27:46 +0800

    add: 给jetty的demo加上websocket演示

--- nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-jetty/src/main/java/io/nutz/demo/simple/websocket/MyWebSocket.java
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-jetty/src/main/java/io/nutz/demo/simple/websocket/MyWebSocket.java
@@ -1,19 +1,47 @@
 package io.nutz.demo.simple.websocket;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.plugins.mvc.websocket.AbstractWsEndpoint;
 import org.nutz.plugins.mvc.websocket.NutWsConfigurator;
 import org.nutz.plugins.mvc.websocket.WsHandler;
 
+import io.nutz.demo.simple.service.OrderService;
+
 @ServerEndpoint(value = "/websocket", configurator=NutWsConfigurator.class)
-@IocBean // 使用NutWsConfigurator的必备条件
+@IocBean(create="init", depose="depose") // 使用NutWsConfigurator的必备条件
 public class MyWebSocket extends AbstractWsEndpoint {
+    
+    @Inject("refer:$ioc")
+    protected Ioc ioc;
+    
+    @Inject
+    protected OrderService orderService;
+    
+    protected ExecutorService es;
 
     public WsHandler createHandler(Session session, EndpointConfig config) {
-        return new MyWsHandler();
+        return new MyWsHandler(ioc, orderService, es);
+    }
+    
+    public void setOrderService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    
+    public void init() {
+        es = Executors.newFixedThreadPool(16);
+    }
+    
+    public void depose() {
+        if (es != null)
+            es.shutdown();
     }
 }
--- nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-jetty/src/main/java/io/nutz/demo/simple/websocket/MyWsHandler.java
+++ nutzboot-demo/nutzboot-demo-simple/nutzboot-demo-simple-jetty/src/main/java/io/nutz/demo/simple/websocket/MyWsHandler.java
@@ -1,7 +1,39 @@
 package io.nutz.demo.simple.websocket;
 
+import java.util.concurrent.ExecutorService;
+
+import org.nutz.ioc.Ioc;
+import org.nutz.lang.util.NutMap;
 import org.nutz.plugins.mvc.websocket.handler.SimpleWsHandler;
+
+import io.nutz.demo.simple.service.OrderService;
 
 public class MyWsHandler extends SimpleWsHandler {
 
+    protected Ioc ioc;
+    protected OrderService orderService;
+    protected ExecutorService es;
+    
+    public MyWsHandler(Ioc ioc, OrderService orderService, ExecutorService es) {
+        super();
+        this.ioc = ioc;
+        this.orderService = orderService;
+        this.es = es;
+    }
+    
+    
+    // 对应js代码 ws.send('{action:"buy", order:1}')
+    public void buy(NutMap params) {
+        // 先检查一下参数
+        Number n = params.getAs("order", Number.class);
+        if (n == null) {
+            // 没提过数量就拒绝咯
+            endpoint.sendJson(session.getId(), new NutMap("ok", false).setv("msg", "没有提供数量"));
+        }
+        else {
+            // 返回处理结果
+            boolean re = orderService.buy(n.intValue());
+            endpoint.sendJson(session.getId(), new NutMap("ok", re));
+        }
+    }
 }
