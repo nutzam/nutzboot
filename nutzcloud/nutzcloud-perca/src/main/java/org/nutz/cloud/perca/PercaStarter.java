6076bc3876109a26f967141c533f4e530baa8425
Wendal Chen <wendal1985@gmail.com>
2020-05-26 09:51:27 +0800

    add: 加个空的PercaStarter

---
+++ nutzcloud/nutzcloud-perca/src/main/java/org/nutz/cloud/perca/PercaStarter.java
@@ -0,0 +1,12 @@
+package org.nutz.cloud.perca;
+
+import org.nutz.boot.starter.ServerFace;
+
+public class PercaStarter implements ServerFace {
+
+	@Override
+	public void start() throws Exception {
+		// nop yet.
+	}
+
+}
