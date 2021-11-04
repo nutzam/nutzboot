086c9ffcdcd9d321339c1952ca2559278468213d
Wizzer <wizzer@qq.com>
2021-11-04 10:10:56 +0800

    update: nutzboot-starter-logback-exts 支持日志文件压缩

--- nutzboot-starter/nutzboot-starter-logback-exts/src/main/java/org/nutz/boot/starter/logback/exts/logfile/LogfileTimeBasedRollingPolicy.java
+++ nutzboot-starter/nutzboot-starter-logback-exts/src/main/java/org/nutz/boot/starter/logback/exts/logfile/LogfileTimeBasedRollingPolicy.java
@@ -7,8 +7,12 @@ public class LogfileTimeBasedRollingPolicy<E> extends TimeBasedRollingPolicy<E> 
     @Override
     public void start() {
         String fileNamePattern = super.getFileNamePattern();
-        if (fileNamePattern.endsWith(".log") || fileNamePattern.endsWith(".LOG")) {
+        if (fileNamePattern.toLowerCase().endsWith(".log")) {
             fileNamePattern = fileNamePattern.substring(0, fileNamePattern.length() - 4) + "-" + Lang.JdkTool.getProcessId("0") + ".log";
+        } else if (fileNamePattern.toLowerCase().endsWith(".gz")) {
+            fileNamePattern = fileNamePattern.substring(0, fileNamePattern.length() - 3) + "-" + Lang.JdkTool.getProcessId("0") + ".gz";
+        } else if (fileNamePattern.toLowerCase().endsWith(".zip")) {
+            fileNamePattern = fileNamePattern.substring(0, fileNamePattern.length() - 4) + "-" + Lang.JdkTool.getProcessId("0") + ".zip";
         } else {
             fileNamePattern = fileNamePattern + "-" + Lang.JdkTool.getProcessId("0") + ".log";
         }
